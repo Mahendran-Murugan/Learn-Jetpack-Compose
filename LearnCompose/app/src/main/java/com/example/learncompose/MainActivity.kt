355f31a1bcb777d0238d7b22c0beda85aef0e402
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-06-07 22:19:29 +0530

    feat: learn modifiers and arrangement

--- LearnCompose/app/src/main/java/com/example/learncompose/MainActivity.kt
+++ LearnCompose/app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learncompose.ui.theme.LearnComposeTheme
 
@@ -34,10 +42,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        color = Color.Gray,
-        fontSize = 30.sp,
+    Column(
         modifier = modifier
-    )
+            .background(Color.White)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = Color.Black,
+            fontSize = 30.sp,
+        )
+        Text(
+            text = "Hello for 2nd time $name!",
+            color = Color.Black,
+            fontSize = 30.sp,
+        )
+    }
 }
