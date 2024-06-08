b81a84a095a2ee182074fcbbdd3441b43b567ba6
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-06-08 10:01:03 +0530

    feat: learn conditional rendering and Image and Icon

--- LearnCompose/app/src/main/java/com/example/learncompose/MainActivity.kt
+++ LearnCompose/app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,13 +15,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,7 +42,7 @@ class MainActivity : ComponentActivity() {
             LearnComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Mahendran",
+                        name = "Mahendran M",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -101,4 +109,43 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 //            fontSize = 30.sp,
 //        )
 //    }
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        val res:String = if(name.length > 10)  "> 10 So Add Displayed" else "< 10 So Android Displayed"
+        Text(
+            text = name,
+            color = Color.Gray,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = "Length is ${name.length} $res",
+            color = Color.Gray,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        if(name.length > 10){
+            Icon(
+                imageVector =  Icons.Default.Add,
+                contentDescription = null,
+                tint = Color.White,
+                modifier = modifier
+                    .background(Color.Green)
+                    .height(60.dp)
+                    .width(60.dp)
+            )
+        }else{
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                modifier = modifier
+                    .background(Color.Yellow)
+            )
+        }
+
+    }
 }
